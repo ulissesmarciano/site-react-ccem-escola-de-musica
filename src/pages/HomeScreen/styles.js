316899116d10f781f01a0993f730cbca67ba2296
Ulisses Gonçalves <ulissesmkt2104@gmail.com>
2024-01-02 14:45:37 -0300

    Adição de folha de estilos para o HomeScreen

---
+++ src/pages/HomeScreen/styles.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    padding: 0 1rem;
+
+    position: relative;
+    z-index: -2;
+    background-color: #FFF;
+
+    @media(min-width: 601px){
+        max-width: 80%;
+        margin: auto;
+    }
+`
