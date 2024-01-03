b04b4d700821c1329ec5df1dd225ae7e18b7c287
Ulisses Gonçalves <ulissesmkt2104@gmail.com>
2024-01-03 16:10:31 -0300

    Modificação na folha de estilos

--- src/pages/HomeScreen/styles.js
+++ src/pages/HomeScreen/styles.js
@@ -3,8 +3,6 @@ import styled from "styled-components";
 export const Container = styled.div`
     padding: 0 1rem;
 
-    position: relative;
-    z-index: -2;
     background-color: #FFF;
 
     @media(min-width: 601px){
