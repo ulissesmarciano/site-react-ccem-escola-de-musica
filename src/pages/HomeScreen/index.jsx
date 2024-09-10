cd4ced3f4a6a8d5ceea01255c0d23af28b7776a8
Ulisses <ulissesmkt2104@gmail.com>
2024-09-10 14:40:23 -0300

    Renomeando componentes

---
+++ src/pages/HomeScreen/index.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Container } from './styles';
+
+import Header from '../../components/Header';
+import GreetingsSection from './sections/greetings';
+import CoursesSection from './sections/courses';
+import Footer from '../../components/Footer';
+
+function HomeScreen() {
+  return (<>
+    <Header/>
+    <Container>
+        <GreetingsSection />
+        <CoursesSection />
+        <Footer />
+    </Container>  
+  </>);
+};
+
+export default HomeScreen;
+
--- src/pages/HomeScreen/styles.js
+++ src/pages/HomeScreen/styles.js
@@ -1,12 +1,12 @@
 import styled from "styled-components";
 
-export const Container = styled.div`
+export const Container = styled.main`
     padding: 0 1rem;
 
     background-color: #FFF;
 
     @media(min-width: 601px){
-        max-width: 80%;
+        //max-width: 80%;
         margin: auto;
     }
 `
