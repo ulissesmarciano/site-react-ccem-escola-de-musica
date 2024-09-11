dfdf0d1f8c6629cb76662b7a6b8ec458063ac8f6
Ulisses <ulissesmkt2104@gmail.com>
2024-09-11 14:16:11 -0300

    Importando novo componente

--- src/pages/HomeScreen/index.jsx
+++ src/pages/HomeScreen/index.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Container } from './styles';
 
 import Header from '../../components/Header';
-import GreetingsSection from './sections/greetings';
+import GreetingsSection from '../../components/GreetingsSection';
 import CoursesSection from './sections/courses';
 import Footer from '../../components/Footer';
 
