636f2dfc2c14b0816e986cb5b30b4129398b20fb
Ulisses <ulissesmkt2104@gmail.com>
2024-11-28 21:02:02 -0300

    Atualização: Correção no tamanho da seção

--- src/components/GreetingsSection/styles.js
+++ src/components/GreetingsSection/styles.js
@@ -4,7 +4,7 @@ import backgroundDesktopImage from "../../assets/images/bg-desktop.svg"
 
 export const Container = styled.section`
 
-    height: calc(100vh - 88.38px);
+    min-height: calc(100vh - 88.38px);
     background: url(${backgroundMobileImage}) top center no-repeat ;
     background-size: 350px;
     
