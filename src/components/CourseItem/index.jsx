7649d8c80191cfcbe0399174491bc589c80683fb
Ulisses <ulissesmkt2104@gmail.com>
2024-11-28 22:56:04 -0300

    Atualização: novo parâmetro para passar uma variante no componente

--- src/components/CourseItem/index.jsx
+++ src/components/CourseItem/index.jsx
@@ -3,11 +3,11 @@ import React from 'react';
 import { Container } from './styles';
 
 
-const CourseItem = ({ title, description, src, alt }) => {
+const CourseItem = ({ title, description, src, alt, variant }) => {
   return (
-    <Container>
-      <div className='course-info-container'>
-        <div>
+    <Container $variant={variant}>
+      <div className='course-container' >
+        <div className='course-info-container'>
           <h3 className='course-title'>{title}</h3>
           <p className='course-description'>{description}</p>
         </div>
