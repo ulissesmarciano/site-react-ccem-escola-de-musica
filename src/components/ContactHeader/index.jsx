112f3ccddf7e30525d8ec1751dd3feb914cd118d
Ulisses <ulissesmkt2104@gmail.com>
2024-12-02 23:46:14 -0300

    Criação: componente de contato acima do header

---
+++ src/components/ContactHeader/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Container } from './styles';
+
+const ContactHeader = () => {
+  return (
+    <Container>
+        <div>
+        CELULAR/WHATSAPP (19) 97749-9665
+        </div>
+        <div></div>
+    </Container>
+  );
+};
+
+export default ContactHeader;
---
+++ src/components/ContactHeader/styles.js
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    background-color: #350030;
+    color: #fff;
+    font-family: 'Poppins', sans-serif;
+    font-weight: 300;
+`
