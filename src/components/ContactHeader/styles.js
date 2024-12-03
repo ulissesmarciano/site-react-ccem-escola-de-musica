788c29c5f50a1585520958712362fa435dbe0319
Ulisses <ulissesmkt2104@gmail.com>
2024-12-03 00:39:34 -0300

    Atualização: Estilização e aplicação de responsividade com o mobile first no componente.

--- src/components/ContactHeader/styles.js
+++ src/components/ContactHeader/styles.js
@@ -1,8 +1,33 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-    background-color: #350030;
-    color: #fff;
-    font-family: 'Poppins', sans-serif;
-    font-weight: 300;
+    padding: .5rem 1rem;
+    
+    
+
+    background-color: #350030;    
+
+    .header-contact-container {
+        display: flex;
+        justify-content: center;
+        flex-wrap: wrap;
+
+        font-family: 'Poppins', sans-serif;
+        text-align: center;
+        color: #fff;
+        font-weight: 300;
+        font-size: 1em;
+        gap: .4rem;
+    }
+
+    @media(min-width: 769px){
+        
+        .header-contact-container {
+            margin: 0 auto;
+            padding: 1rem;
+            max-width: 1440px;
+            
+            justify-content: space-between;
+        }
+    }
 `
