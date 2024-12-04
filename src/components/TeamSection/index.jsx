871ea1e3eaab31266d57d50bf36c110e65d3723e
Ulisses <ulissesmkt2104@gmail.com>
2024-12-04 11:10:33 -0300

    Criação: Componente criado para renderizar a equipe de professsores

---
+++ src/components/TeamSection/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Container } from './styles';
+
+const TeamSection = () => {
+  return (
+    <Container>
+      TeamSection
+    </Container>
+  )
+}
+
+export default TeamSection;
