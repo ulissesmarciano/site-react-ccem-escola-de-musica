44d7cbf9bbd739d9e66528864653b203304ebde5
Ulisses <ulissesmkt2104@gmail.com>
2024-12-04 11:20:10 -0300

    Criação: componente para a construção dos profissionais na página

---
+++ src/components/TeamProfessionalItem/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Container } from './styles';
+
+const TeamProfessionalItem = () => {
+  return (
+    <Container>
+      Professional
+    </Container>
+  );
+};
+
+export default TeamProfessionalItem;
