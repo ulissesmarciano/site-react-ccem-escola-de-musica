3f8c383c9b55a5bb0250e398024ecfbf855a6be6
Ulisses <ulissesmkt2104@gmail.com>
2024-12-04 11:21:20 -0300

    Atualização: Importação do novo componente na seção

--- src/components/TeamSection/index.jsx
+++ src/components/TeamSection/index.jsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import { Container } from './styles';
+import TeamProfessionalItem from '../TeamProfessionalItem';
 
 const TeamSection = () => {
-  return (
-    <Container>
-      TeamSection
-    </Container>
-  )
-}
+    return (
+        <Container>
+            <div className='section-title'>
+                <p className='section-title' id='cursos'>Conheça</p>
+                <h2>Nosso Time</h2>
+            </div>
+            <TeamProfessionalItem />
+            <TeamProfessionalItem />
+            <TeamProfessionalItem />
+            <TeamProfessionalItem />
+        </Container>
+    );
+};
 
 export default TeamSection;
--- src/components/TeamSection/styles.js
+++ src/components/TeamSection/styles.js
@@ -1,5 +1,41 @@
 import styled from "styled-components";
 
 export const Container = styled.section`
+    margin: 4rem 0;
+    padding: 0 1rem;
     
+    .section-title p{
+        margin: 0 0 .6rem;
+        scroll-margin-top: 100px;
+        
+        font-size: 1.4rem;
+        font-weight: 400;
+        text-transform: uppercase;
+        color: #b96646;
+    }
+
+    .section-title h2 {
+        margin: 0 0 4rem;
+
+        font-size: 2rem;
+        font-weight: 400;
+        text-transform: uppercase;
+        color: #350030;
+    }
+
+    @media (min-width: 601px){
+        
+        margin: auto;
+        padding: 1rem;
+        max-width: 1440px;
+
+        .section-title p {
+            font-size: 2.2rem;
+        }
+
+        .section-title h2 {
+            font-size: 3.5rem;
+        }
+
+    }
 `
