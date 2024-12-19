54b865cef9811b1d8708772777824cc5e3b0c68c
Ulisses <ulissesmkt2104@gmail.com>
2024-12-18 22:50:01 -0300

    Atualização: Adicionando novos parâmetros para renderização do link em outras seções

--- src/components/LinkItem/index.jsx
+++ src/components/LinkItem/index.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { LinkContainer } from './styles';
 
-const LinkItem = ({variant, href, name, onClick}) => {
+const LinkItem = ({ variant, href, name, onClick, target, src, alt }) => {
   return (
-    <LinkContainer $variant={variant} href={href} onClick={onClick}>
-      <p>{name}</p>
+    <LinkContainer $variant={variant} href={href} target={true ? target : !target} onClick={onClick}>
+      <p>{true ? name : !name}</p>
+      <img src={true ? src : !src} alt={alt} />
     </LinkContainer>
   );
 };
