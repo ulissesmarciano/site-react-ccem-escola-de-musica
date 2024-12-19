6ad60b443f477f7dda9da8211aeec33054d53da9
Ulisses <ulissesmkt2104@gmail.com>
2024-12-19 15:56:52 -0300

    Criação: Novo objeto contendo contatos da empresa

---
+++ src/constants/contactLinks.js
@@ -0,0 +1,5 @@
+export const contactLinks = {
+    whatsappHref: "https://api.whatsapp.com/send?phone=5513996484716&text=Ol%C3%A1.%20Quero%20conhecer%20a%20escola!",
+    instagramHref: "https://www.instagram.com/ulissesmarciano/",
+    facebookHref: "https://www.facebook.com/profuliisses",
+}
