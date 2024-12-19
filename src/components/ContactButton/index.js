d2282eb4f2cef54f37c735ca4573b40b1ec1c381
Ulisses <ulissesmkt2104@gmail.com>
2024-12-18 22:28:48 -0300

    Atualização: Formatação de código

--- src/components/ContactButton/index.js
+++ src/components/ContactButton/index.js
@@ -4,7 +4,7 @@ import { Container } from './styles'
 function ContactButton() {
   return (
     <Container href='https://api.whatsapp.com/send?phone=5519993807111&text=Ol%C3%A1.%20Quero%20conhecer%20a%20escola!' target='blank'>
-        contato
+      contato
     </Container>
   )
 }
